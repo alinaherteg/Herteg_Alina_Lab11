c3b32037d3c804067379971fffa1fdce138f154e
Alina <Alina@DESKTOP-NB81HVC>
2023-01-04 13:51:10 +0200

    Add project files.

---
+++ Herteg_Alina_Lab11/Models/ShopList.cs
@@ -0,0 +1,9 @@
+﻿namespace Herteg_Alina_Lab11.Models
+{
+    public class ShopList
+    {
+        public int ID { get; set; }
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
